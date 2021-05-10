66ee595fe7ec080258ce44fe1978d51f76cb6960
Colle <colleaguexluffy@gmail.com>
2021-05-10 16:59:52 +0200

    image (nein)

--- java/src/Controller/Controller.java
+++ java/src/Controller/Controller.java
@@ -2,8 +2,18 @@ package Controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Controller {
     @FXML
@@ -19,7 +29,24 @@ public class Controller {
     @FXML
     private AnchorPane paneBoard;
 
+    public void initialize() throws IOException {
+
+        /*
+        FileInputStream inputStream = new FileInputStream("..\\View\\images\\Projekt_-Kase_Kopie.png");
+        Image image = new Image(inputStream);
+        imageMouse1 = new ImageView(image); // new Image("../View/images/Projekt_-Kase_Kopie.png"));
+         */
 
 
 
+
+    }
+
+
+    public void onMouseClickedTimer(MouseEvent mouseEvent) throws IOException {
+        System.out.println("Klick");
+        BufferedImage image = ImageIO.read(new File("../View/images/Projekt_-Maus_Kopie.png"));
+        Image imageI = new Image(String.valueOf(image));
+        imageMouse1 = new ImageView(imageI); // new Image("../View/images/Projekt_-Maus_Kopie.png"));
+    }
 }
