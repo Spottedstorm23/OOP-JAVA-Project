a8db9bee9c6fd39ff30e8f0ef394870838b2085e
Colle <colleaguexluffy@gmail.com>
2021-05-10 15:50:39 +0200

    bissi bei FXML: alttexts, fx:ids etc

--- java/src/Controller/Controller.java
+++ java/src/Controller/Controller.java
@@ -1,4 +1,25 @@
 package Controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class Controller {
+    @FXML
+    private ImageView imageMouse1;
+    @FXML
+    private ImageView imageMouse2;
+    @FXML
+    private ImageView imageMouse3;
+
+    @FXML
+    private Label labelCurrentMode;
+
+    @FXML
+    private AnchorPane paneBoard;
+
+
+
+
 }
