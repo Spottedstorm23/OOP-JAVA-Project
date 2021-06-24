5734b9517dc78348a9504fe4f383a17baa949321
Cora Stokowsky <cv.stokowsky@gmx.de>
2021-06-24 19:10:11 +0200

    Visualization split in 3 functions, Mouse and Cheese got own layer

--- java/src/Main/Main.java
+++ java/src/Main/Main.java
@@ -1,5 +1,6 @@
 package Main;
 
+import Controller.Controller;
 import Controller.Maps;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,6 +8,8 @@ import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
@@ -22,6 +25,10 @@ import java.util.Scanner;
 
 public class Main extends Application {
 
+    //Important
+    int mouseX;
+    int mouseY;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../View/Menu.fxml")); // Geht aus dem Main-Package raus und dann ins View-Package rein
@@ -29,13 +36,15 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root));
 
-        /*
-        StackPane rootlvl = new StackPane();
-        Scene level = new Scene(rootlvl,950,650);
+
+        Pane levelPane = new Pane();
+        Scene level = new Scene(levelPane, 950, 650);
         primaryStage.setScene(level);
         Maps map = new Maps();
-        updateLvl(rootlvl, map.getMap());
-         */
+        byte [][] levelmap = map.getMap();
+        drawLvl(levelPane, levelmap);
+        updateCheese(levelPane, levelmap);
+        drawMouse(levelPane);
 
         primaryStage.show();
     }
@@ -107,46 +116,112 @@ public class Main extends Application {
         }
     }
 
-    public void updateLvl(StackPane root, byte[][] map){
-        //Shows Level
+    public void drawLvl(Pane root, byte[][] map) {
+        //Draws Level -> only Path and Wall
         //by Cora
         Group group = new Group();
-        int x = 0;
-        for(x = 0; x < 13; x++){
-            int y = 0;
-            for(y = 0; y < 19; y++){
-                int cord_y = x*50;
-                int cord_x = y*50;
-                Rectangle r = new Rectangle(cord_x,cord_y,50,50);
+        int y = 0;
+        for (y = 0; y < 13; y++) {
+            int x = 0;
+            for (x = 0; x < 19; x++) {
+                int cord_y = y * 50;
+                int cord_x = x * 50;
+                Rectangle r = new Rectangle(cord_x, cord_y, 50, 50);
 
-                switch (map[x][y]){
+                switch (map[y][x]) {
                     case 0: {
-                        r.setFill(Color.color(20/ 255.0F,47/ 255.0F,82/ 255.0F));
+                        // Case for Wallcolor
+                        r.setFill(Color.color(20 / 255.0F, 47 / 255.0F, 82 / 255.0F));
                         break;
                     }
-                    case 1:{
-                        r.setFill(Color.color(221/ 255.0F,227/ 255.0F,236/ 255.0F));
+                    case 4: {
+                        // sets first Mouse Coordinates
+                        r.setFill(Color.color(221 / 255.0F, 227 / 255.0F, 236 / 255.0F));
+                        setMouseX(cord_x);
+                        setMouseY(cord_y);
                         break;
                     }
-                   case 2:{ Image cheese = new Image("./View/images/Projekt_-_KaeseHG.png");
-                       r.setFill(new ImagePattern(cheese));
-                       break;
-                    }
-                    case 3:{ Image cheesepile = new Image("./View/images/Projekt_-_KaesehaufenHG.png");
-                       r.setFill(new ImagePattern(cheesepile));
-                       break;
-                    }
-                    //Just for Visualisierung, bis wir wissen wie wir die Maus implementieren wollen
-                    case 4:{Image mouse = new Image("./View/images/Projekt_-_MausHG.png");
-                        r.setFill(new ImagePattern(mouse));
+                    default: {
+                        // Case for Pathcolor
+                        r.setFill(Color.color(221 / 255.0F, 227 / 255.0F, 236 / 255.0F));
                         break;
                     }
                 }
                 group.getChildren().add(r);
+
             }
             System.out.println("");
         }
         root.getChildren().add(group);
+    }
+
+    public void updateCheese(Pane root, byte[][] map) {
+        //Updates Cheespiles on the Map
+        //by Cora
+        Group groupCheese = new Group();
+        int y = 0;
+        for (y = 0; y < 13; y++) {
+            int x = 0;
+            for (x = 0; x < 19; x++) {
+                int cord_y = y * 50;
+                int cord_x = x * 50;
+
+                switch (map[y][x]) {
+                    case 2: {
+                        Rectangle r_cheese = new Rectangle(cord_x, cord_y, 50, 50);
+                        Image cheese = new Image("./View/images/Projekt_-_Kaese.png");
+                        r_cheese.setFill(new ImagePattern(cheese));
+                        groupCheese.getChildren().add(r_cheese);
+                        break;
+                    }
+                    case 3: {
+                        Rectangle r_cheese = new Rectangle(cord_x, cord_y, 50, 50);
+                        Image cheesepile = new Image("./View/images/Projekt_-_Kaesehaufen.png");
+                        r_cheese.setFill(new ImagePattern(cheesepile));
+                        groupCheese.getChildren().add(r_cheese);
+                        break;
+                    }
+                }
+            }
+            System.out.println("");
+        }
+        root.getChildren().add(groupCheese);
+    }
+
+
+    public void drawMouse(Pane root) {
+        // Draws the Mouse on Current position
+        //by Cora
+        int mouseX = getMouseX();
+        int mouseY = getMouseY();
+        Rectangle r_mouse = new Rectangle(mouseX, mouseY, 50, 50);
+        Image mousepic = new Image("./View/images/Projekt_-_Maus.png");
+        r_mouse.setFill(new ImagePattern(mousepic));
+        root.getChildren().add(r_mouse);
+    }
+
+
+    public int setMouseX(int x) {
+        //sets Mouse Coordinate X
+        this.mouseX = x;
+        return mouseX;
+    }
+
+    public int getMouseX() {
+        //returns Mouse Coordinate X
+        return mouseX;
+    }
+
+
+    public int setMouseY(int y) {
+        //sets Mouse Coordinate X
+        this.mouseY = y;
+        return mouseY;
+    }
+
+    public int getMouseY() {
+        //returns Mouse Coordinate Y
+        return mouseY;
     }
 
     public static void main(String[] args) {
