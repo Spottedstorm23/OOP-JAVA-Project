665ffb3619a406b3fc85eb6b8c2bebdca9ad16e2
Maximilian Holtz <s3004015@ba-sachsen.de>
2021-07-06 10:07:16 +0200

    WIP MUSIC SCHEI?E

--- java/src/Music/Music.java
+++ java/src/Music/Music.java
@@ -41,28 +41,49 @@ public class Music {
 
 
   }).start();}
-*/
+
+  Unterschied ist nur:
+Clip audioClip = ...
+und dann später
+audioClip.loop() ← damit es eine Endlosschleife macht
+
+ */
+
+
 
     private AudioStream audioStream;
+    private AudioPlayer audioPlayer;
+
 
     public void playMusic(Boolean isPlaying) throws IOException {
 
         if (isPlaying){
             try {
                 audioStream = new AudioStream(new FileInputStream("java/src/Music/data/Here_Come_The_Raindrops_-_Reed_Mathis.wav"));
-                AudioPlayer.player.start(audioStream);//lässt starten
+              getAudioPlayer().player.start(audioStream);//lässt starten
                 System.out.println("going");
             } catch (IOException e) {
                 System.err.println("music isn't Working :(");
             }
         }
         if (!isPlaying) {
-            if (audioStream != null) {
-                AudioPlayer.player.stop(audioStream);
+            System.out.println("gehts ?");
+     
+            if (audioStream instanceof AudioStream ) {
+                getAudioPlayer().player.stop(audioStream);
                 System.out.println("stopping");
             }
             System.out.println("stopping2");
         }
+    }
+
+    private AudioStream getAudioStream() {
+        return audioStream;
+    }
+
+
+    private AudioPlayer getAudioPlayer() {
+        return this.audioPlayer;
     }
 
 
