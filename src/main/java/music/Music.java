7918068356a12f9c4025952806d01a6fea9dca3c
Cora Stokowsky <cv.stokowsky@gmx.de>
2021-08-06 12:29:24 +0200

    Fix music error (with bufferdStream)

--- src/main/java/music/Music.java
+++ src/main/java/music/Music.java
@@ -1,7 +1,9 @@
 package music;
 
 import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class Music {
     AudioInputStream inputStream;
@@ -9,19 +11,20 @@ public class Music {
 
     public Music() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         // TODO: Caused by: java.io.IOException: mark/reset not supported -> provide stream which supports mark/reset
-        //inputStream = AudioSystem.getAudioInputStream(Music.class.getClassLoader().getResourceAsStream("music/Here_Come_The_Raindrops_-_Reed_Mathis.wav"));
-        //this.clip = AudioSystem.getClip();
-        //clip.open(inputStream);
-        //clip.loop(Clip.LOOP_CONTINUOUSLY);
+        InputStream bufferedInput = new BufferedInputStream(this.getClass().getClassLoader().getResourceAsStream("music/Here_Come_The_Raindrops_-_Reed_Mathis.wav"));
+        inputStream = AudioSystem.getAudioInputStream(bufferedInput);
+        this.clip = AudioSystem.getClip();
+        clip.open(inputStream);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 
     // https://www.geeksforgeeks.org/play-audio-file-using-java/
     // Method to stop the audio
     public void stop() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        //clip.stop();
+        clip.stop();
     }
 
     public void start() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        //clip.loop(Clip.LOOP_CONTINUOUSLY);
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
     }
 }
